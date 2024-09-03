1e431770df9932adfa6774914723c74f4ca75cff
JanVog <janv1999@outlook.com>
2024-09-03 15:55:05 +0200

    Made more minimal

--- Source/MassBugReproducer/FixedTickMassProcessor.cpp
+++
@@ -1,9 +0,0 @@
-#include "FixedTickMassProcessor.h"
-
-
-#include UE_INLINE_GENERATED_CPP_BY_NAME(FixedTickMassProcessor)
-
-void UFixedTickMassProcessor::ConfigureQueries()
-{
-	// ProcessorRequirements.AddSubsystemRequirement<UFixedTickSubsystem>(EMassFragmentAccess::ReadWrite);
-}
--- Source/MassBugReproducer/MyProcessor.h
+++ Source/MassBugReproducer/MyProcessor.h
@@ -1,22 +1,55 @@
 ﻿#pragma once
 
-#include "FixedTickMassProcessor.h"
+#include "MassProcessor.h"
+#include "MassExternalSubsystemTraits.h"
+#include "MassExecutionContext.h"
+#include "MassSubsystemBase.h"
 #include "MyProcessor.generated.h"
 
 
 UCLASS()
-class MASSBUGREPRODUCER_API UMyProcessorA : public UFixedTickMassProcessor
+class MASSBUGREPRODUCER_API UMySubsystem : public UMassSubsystemBase
+{
+	GENERATED_BODY()
+
+protected:
+	virtual bool DoesSupportWorldType(const EWorldType::Type WorldType) const override final { return WorldType == EWorldType::Game || WorldType == EWorldType::PIE; }
+};
+
+template <>
+struct TMassExternalSubsystemTraits<UMySubsystem> final
+{
+	enum
+	{
+		GameThreadOnly = true
+	};
+};
+
+UCLASS()
+class MASSBUGREPRODUCER_API UMyProcessorA : public UMassProcessor
 {
 	GENERATED_BODY()
 
 public:
-	UMyProcessorA();
+	UMyProcessorA()
+	{
+		// ExecutionFlags = static_cast<int32>(EProcessorExecutionFlags::AllNetModes);
+		bAutoRegisterWithProcessingPhases = true;
+		bRequiresGameThreadExecution = false;
+	}
+	
 
 protected:
-	virtual void ConfigureQueries() override;
-	virtual void Execute(FMassEntityManager &EntityManager, FMassExecutionContext &Context) override;
+	virtual void ConfigureQueries() override
+	{
+		ProcessorRequirements.AddSubsystemRequirement<UMySubsystem>(EMassFragmentAccess::ReadWrite);
+	}
 
-	FMassEntityQuery EntityQuery;
+	virtual void Execute(FMassEntityManager &EntityManager, FMassExecutionContext &Context) override
+	{
+		UMySubsystem &MySubsystem = Context.GetMutableSubsystemChecked<UMySubsystem>();
+		check(IsInGameThread());
+	}
 
 	virtual bool ShouldAllowQueryBasedPruning(const bool bRuntimeMode = true) const override { return false; }
 };
